ba12cc2e9bf05fac8fdb9d54ae2abb2529d1d62e
Bruk Lemma <bruklemma2017@gmail.com>
2022-06-06 15:31:01 +0300

    created the Basic MVC structure

---
+++ controllers/teacherController.js
@@ -0,0 +1,30 @@
+exports.getAllTeachers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
+};
+exports.getTeacher = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
+};
+exports.createTeacher = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
+};
+exports.updateTeacher = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
+};
+exports.deleteTeacher = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
+};
---
+++ routes/clientRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const clientController = require('../controllers/clientController');
+
+const router = express.Router();
+
+router.param('id', clientController.checkID);
+
+router
+  .route('/')
+  .get(clientController.getAllClients)
+  .post(clientController.checkBody, clientController.createClient);
+
+router
+  .route('/:id')
+  .get(clientController.getClient)
+  .patch(clientController.updateClient)
+  .delete(clientController.deleteClient);
+
+module.exports = router;
