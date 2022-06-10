e6eaf1d3f67b265ea9c59d812f5ecc636ce70366
Bruk Lemma <bruklemma2017@gmail.com>
2022-06-10 15:56:27 +0300

    connected the app to Cloud-Atlas and LocatMongodb Database

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
+const mongoose=require('mongoose');
 const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const DB=process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+//.connect(process.env.DATABASE_LOCAL,{
+.connect(DB,{
+  useNewUrlParser:true,
+  useCreateIndex:true,
+  useFindAndModify:false,
+  useUnifiedTopology:true
+}).then(con=>{
+  //console.log(con.connections);
+  console.log("DATABASE connection successfull!....");
+});
+
+
+
 const app = require('./app');
 
-//dotenv.config({ path: './config.env' });
-
+//console.log(process.env);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
