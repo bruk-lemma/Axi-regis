99142981742b8b41838f8ca42d3d9b069b7e3f54
Bruk Lemma <bruklemma2017@gmail.com>
2022-06-11 22:12:47 +0300

    added the first test model with data to the database using the save methode

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const DB=process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -18,10 +19,49 @@ mongoose
   console.log("DATABASE connection successfull!....");
 });
 
+const clientSchema=new mongoose.Schema({
+  first_name:{
+    type:String,
+    require:[true,"A client must have a first-name"]
+  },
+  last_name:{
+    type:String,
+    require:[true,"A client must have a first-name"]
+  },
+  address:{
+    type:String,
+    require:[true,"A client must have a first-name"]
+  },
+  phone_number:{
+    type:String,
+    require:[true,"A client must have a first-name"]
+  },
+  child_grade:{
+    type:String,
+    require:[true,"A client must have a first-name"]
+  },
+  child_gender:{
+    type:String,
+    require:[true,"A client must have a first-name"]
+  },
+});
 
 
-const app = require('./app');
+const Client=mongoose.model('Client',clientSchema);
+const testClient=new Client({
+  first_name:"Amdom",
+  last_name:"gkidan",
+  address:"Moenko motors",
+  phone_number:"4545454545",
+  child_grade:"middle school",
+  child_gender:"male"
+});
 
+testClient.save().then(doc=>{
+  console.log(testClient);
+}).catch(err=>{
+  console.log(`Error...${err}`);
+});
 //console.log(process.env);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
