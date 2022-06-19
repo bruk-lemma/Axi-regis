3d2f2e35470011fc26860607c310517da6dd0c60
Bruk Lemma <bruklemma2017@gmail.com>
2022-06-20 00:58:59 +0300

    modified the client model

--- models/clientModel.js
+++ models/clientModel.js
@@ -1,30 +1,42 @@
 const mongoose=require('mongoose');
+const { locals } = require('../app');
 const clientSchema=new mongoose.Schema({
     first_name:{
       type:String,
       required:[true,"A client must have a first-name"],
-      unique: true
+      unique: true,
+      trim:true
     },
     last_name:{
       type:String,
-      required:[true,"A client must have a last-name"]
+      required:[true,"A client must have a last-name"],
+      trim:true
     },
     address:{
       type:String,
-      required:[true,"A client must have a address"]
+      required:[true,"A client must have a address"],
+      trim:true
+
     },
     phone_number:{
       type:String,
-      required:[true,"A client must have a phone number"]
+      required:[true,"A client must have a phone number"],
+      trim:true
     },
     child_grade:{
       type:String,
-      required:[true,"A client must have a child grade"]
+      required:[true,"A client must have a child grade"],
+      trim:true
     },
     child_gender:{
       type:String,
-      required:[true,"A client must have a child gender"]
+      required:[true,"A client must have a child gender"],
+      trim:true
     },
+    createdAt:{
+        type:Date,
+        default:Date.now().toPrecision
+    } 
   });
 const Client=mongoose.model('Client',clientSchema);
 module.exports=Client;
