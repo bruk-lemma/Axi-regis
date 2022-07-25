79a18ceb712c87e606da441453c59fec90c3533b
Bruk Lemma <bruklemma2017@gmail.com>
2022-07-25 16:18:59 +0300

    implemented data sanitization and cross-site-scripting

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit=require("express-rate-limit");
 const helmet=require("helmet"); 
+const mongoSanitize=require('express-mongo-sanitize');
+const xss=require("xss-clean");
 const clientRouter = require('./routes/clientRoutes');
 const teacherRouter = require('./routes/teacherRoutes');
 const userRouter=require("./routes/userRoutes");
@@ -27,6 +29,14 @@ const limiter=rateLimit({
 
 app.use("/api",limiter);
 app.use(express.json({limit:'10kb'}));
+
+//data sanitization against no-sql query injection
+app.use(mongoSanitize());
+
+
+//data sanitization against XSS 
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
